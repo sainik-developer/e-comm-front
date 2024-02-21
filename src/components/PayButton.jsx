59b21d0c9d06ece5b1f6493864dcc39490adf24a
sainik <sainikchattapadhyay@gmail.com>
2024-02-21 21:12:02 +0530

    Header added to checkout call

--- src/components/PayButton.jsx
+++ src/components/PayButton.jsx
@@ -1,29 +1,35 @@
 import axios from "axios";
-import { useSelector } from "react-redux";
-import { url } from "../slices/api";
+import {useSelector} from "react-redux";
+import {url} from "../slices/api";
 
-const PayButton = ({ cartItems }) => {
-  const user = useSelector((state) => state.auth);
+const PayButton = ({cartItems}) => {
+    const user = useSelector((state) => state.auth);
+    const token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJfaWQiOiJzYWluaWtAZ21haWwuY29tIiwibmFtZSI6InNhaW5pa0BnbWFpbC5jb20iLCJlbWFpbCI6InNhaW5pa0BnbWFpbC5jb20ifQ.xcMuqyKjp9hCWq-tKyxQ8bbC40oYSMRRceKpmxkUNJA'
 
-  const handleCheckout = () => {
-    axios
-      .post(`${url}/stripe/create-checkout-session`, {
-        cartItems,
-        userId: user._id,
-      })
-      .then((response) => {
-        if (response.data.url) {
-          window.location.href = response.data.url;
-        }
-      })
-      .catch((err) => console.log(err.message));
-  };
+    const handleCheckout = () => {
+        axios
+            .post(`${url}/stripe/create-checkout-session`, {
+                cartItems,
+                userId: user._id,
+            }, {
+                headers: {
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJfaWQiOiJzYWluaWtAZ21haWwuY29tIiwibmFtZSI6InNhaW5pa0BnbWFpbC5jb20iLCJlbWFpbCI6InNhaW5pa0BnbWFpbC5jb20ifQ.xcMuqyKjp9hCWq-tKyxQ8bbC40oYSMRRceKpmxkUNJA'
+                }
+            })
 
-  return (
-    <>
-      <button onClick={() => handleCheckout()}>Check out</button>
-    </>
-  );
+            .then((response) => {
+                if (response.data.url) {
+                    window.location.href = response.data.url;
+                }
+            })
+            .catch((err) => console.log(err.message));
+    };
+
+    return (
+        <>
+            <button onClick={() => handleCheckout()}>Check out</button>
+        </>
+    );
 };
 
 export default PayButton;
